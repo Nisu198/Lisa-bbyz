a0bf05f19f06b1e84b0b7f3441813fd0cf4053a5
Nisu198 <gohcppufuovhoh@gmail.com>
2024-07-27 19:01:28 +0600

    Update photoxy.js

--- scripts/commands/photoxy.js
+++ scripts/commands/photoxy.js
@@ -1,7 +1,7 @@
 module.exports.config = {
   name: "photoxy",
   version: "2.0.0",
-  permission: 2,
+  permission: 0,
   credits: "Nayan",
   description: "",
   prefix: true,
@@ -75,7 +75,7 @@ module.exports.run = async function({ api, event, args }) {
     setTimeout(function() {
 
       return api.sendMessage({
-        body: `❐ THIS IS YOUR NAME EDIT ✌️\n\n___________________________________\n\n❐ This Bot Name : ${global.config.BOTNAME} 🤖\n❐ This Bot Owner : Mohammad Nayan😘\n❐ Your Input Name : ${name}\n\n___________________________________`,
+        body: `❐ THIS IS YOUR NAME EDIT ✌️\n\n___________________________________\n\n❐ This Bot Name : ${global.config.BOTNAME} 🤖\n❐ This Bot Owner : NisanN😘\n❐ Your Input Name : ${name}\n\n___________________________________`,
         attachment: fs.createReadStream(__dirname + '/cache/photoxy.jpg')
       }, threadID, messageID)
     }, 5000)
@@ -83,6 +83,6 @@ module.exports.run = async function({ api, event, args }) {
     } catch (err) {
    api.setMessageReaction("❌", event.messageID, (err) => {
   }, true);
-    api.sendMessage(`🔰Use ${global.config.PREFIX}${this.config.name} [no.] [text]\n🔰Example:${global.config.PREFIX}${this.config.name} 1 nayan\n\n🔥Total Edit limit 25...`, event.threadID, event.messageID);  
+    api.sendMessage(`🔰Use ${global.config.PREFIX}${this.config.name} [no.] [text]\n🔰Example:${global.config.PREFIX}${this.config.name} 1 nisan\n\n🔥Total Edit limit 25...`, event.threadID, event.messageID);  
    }
 };
